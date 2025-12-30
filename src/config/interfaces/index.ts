dc6c5311f4d212b24882e3f341fe11128e5a57a9
alexanu4 <alexdmarchis@gmail.com>
2025-12-30 09:06:56 +0200

    Revert "I have covered this sections from nest docs:"
    This reverts commit dfb4eb55c12952157a58b2d62e6cf16c0936d531.

--- src/config/interfaces/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './envconfig.interface';
-export * from './config-options.interface';
-export * from './config-module-options.interface';
--- src/decorator/cache.decorator.ts
+++
@@ -1,3 +0,0 @@
-import { Reflector } from '@nestjs/core';
-
-export const CacheKey = Reflector.createDecorator<string>();
